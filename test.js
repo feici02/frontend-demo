827e89819653f149c8707f3a1d8bc559ccc23c15
feici02 <wangxu1982@gmail.com>
2017-01-05 16:12:41 +0800

    initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+function toLower() {
+	document.getElementById("demo").innerHTML = "hello, world!";
+}
+function toUpper() {
+	document.getElementById("demo").innerHTML = "HELLO, WORLD!";
+}
+function toRestore() {
+	document.getElementById("demo").innerHTML = "HeLlO, wOrLd!";
+}
